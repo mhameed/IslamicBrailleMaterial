c7f5e9da791a998fd63ed2500c3ef0a5fb8070a7
Mesar Hameed <mesar.hameed@gmail.com>
2019-04-05 23:41:34 +0100

    Remove additional chars

--- Quran/step2-remove.sh
+++ Quran/step2-remove.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This step removes  shadda, sokon, and the three types of tanween.
 sed -e 's/ْ//g' -e 's/ً//g' -e 's/ٌ//g' -e 's/ٍ//g' -e 's/ّ//g'
+-e 's/!//g' -e 's/،//g' # we dont need esclames or arabic commas.
